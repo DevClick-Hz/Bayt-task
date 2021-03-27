14693bea992be2f9eed43da6df89f8351d36b8c1
Hamza Zaitoun <49735537+DevClick-Hz@users.noreply.github.com>
2021-03-27 17:12:19 +0300

    Bayt-task
    finishing the project

---
+++ src/App.vue
@@ -0,0 +1,195 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <div class="tabs">
+        <h4 class="logo"><i class="fas fa-home"></i> Logo</h4>
+        <div :class="openMenu ? 'open' : 'links'">
+          <router-link to="/">Team</router-link>
+          <router-link to="/">Work</router-link>
+          <router-link to="/">Price</router-link>
+          <router-link to="/">Contact</router-link>
+          <div class="dropdown">
+            <button class="dropbtn">
+              Dropdown
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="#">dumy text</a>
+              <a href="#">dumy text</a>
+              <a href="#">dumy text</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="search">
+        <h4><i class="fas fa-search"></i></h4>
+      </div>
+      <div class="ham" @click="opened">
+        <h4><i class="fas fa-bars"></i></h4>
+      </div>
+    </div>
+    <router-view />
+    <div class="foot">
+      <i class="fas fa-chevron-circle-up" @click="toTop"></i>
+      <p>follow us</p>
+      <i class="fab fa-facebook-f"></i>
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-google-plus-g"></i>
+      <i class="fab fa-instagram"></i>
+      <i class="fab fa-linkedin-in"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      openMenu: false,
+    };
+  },
+  methods: {
+    opened() {
+      this.openMenu = !this.openMenu;
+    },
+    toTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    },
+  },
+};
+</script>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+h4 {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #ffffff;
+  padding: 0 1rem 0 0;
+  position: relative;
+}
+
+#nav a {
+  color: #ffffff;
+  margin: 0 0.5rem;
+  padding: 0.7rem;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #ffffff;
+}
+.tabs,
+.links {
+  display: flex;
+  align-items: center;
+}
+.logo {
+  background-color: rgb(0, 119, 119);
+  padding: 0.7rem 1rem;
+}
+.ham {
+  display: none;
+}
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;
+  border: none;
+  outline: none;
+  color: white;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #000000;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  padding: 0.5rem 0;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+.search {
+  cursor: pointer;
+}
+.foot {
+  background-color: black;
+  color: white;
+  padding: 3rem 0;
+  position: relative;
+}
+.foot .fab {
+  padding: 0.7rem 0.5rem;
+  background-color: rgb(0, 119, 119);
+  width: 25px;
+  margin: 0 0.3rem;
+  cursor: pointer;
+}
+.foot p {
+  font-size: 1.2rem;
+}
+.foot .fas {
+  position: absolute;
+  right: 1rem;
+  top: -20px;
+  padding: 1rem;
+  background-color: black;
+  cursor: pointer;
+}
+/* ------- media query -------*/
+@media screen and (max-width: 600px) {
+  .search,
+  .dropbtn,
+  .links {
+    display: none;
+  }
+  .open {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    background-color: black;
+    padding: 1rem 0;
+    top: 40.3px;
+  }
+  .ham {
+    display: block;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="home">
+    <div class="head"></div>
+    <div id="team">
+      <h2>OUR TEAM</h2>
+      <h4>meet the team - our office rats:</h4>
+      <div class="theTeam">
+        <div class="person item" v-for="(guy, index) in team" :key="index">
+          <img :src="guy.pic" alt="" />
+          <h3>{{ guy.name }}</h3>
+          <p>{{ guy.position }}</p>
+        </div>
+      </div>
+    </div>
+    <div id="work">
+      <div class="work-desc item">
+        <h2>Our Work</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div class="work-card item" v-for="(card, index) in cards" :key="index">
+        <img :src="card.pic" alt="" />
+        <div class="card-text">
+          <h3>{{ card.title }}</h3>
+          <p>dumy text</p>
+          <p>dumy text</p>
+          <p>dumy text</p>
+          <p>dumy text</p>
+          <p>dumy text</p>
+        </div>
+      </div>
+      <div class="fas">+</div>
+    </div>
+    <div id="contact">
+      <div class="info item">
+        <h2>Contact Us</h2>
+        <h3>Address</h3>
+        <p>swing by for a cup of coffee or whatever.</p>
+        <p><i class="fas fa-map-marker-alt"></i> Amman,Jordan</p>
+        <p><i class="fas fa-phone-alt"></i> +96279000000</p>
+        <p><i class="far fa-envelope"></i> test@test.com</p>
+      </div>
+      <div class="form item">
+        <label for="Name">Name</label>
+        <input type="text" />
+        <label for="Email">Email</label>
+        <input type="text" />
+        <label for="Message">Message</label>
+        <input type="text" />
+        <div class="sub">
+          <div class="box">
+            <input type="checkbox" name="" id="" />
+            <label for="like">I like it!</label>
+          </div>
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      team: [
+        {
+          name: "Ahmad Naser",
+          position: "Support",
+          pic: require("../assets/guy.jpg"),
+        },
+        {
+          name: "Ahmad Naser",
+          position: "Support",
+          pic: require("../assets/guy.jpg"),
+        },
+        {
+          name: "Ahmad Naser",
+          position: "Support",
+          pic: require("../assets/guy.jpg"),
+        },
+        {
+          name: "Ahmad Naser",
+          position: "Support",
+          pic: require("../assets/guy.jpg"),
+        },
+      ],
+      cards: [
+        { pic: require("../assets/card.jpg"), title: "Customer 1" },
+        { pic: require("../assets/card.jpg"), title: "Customer 2" },
+        { pic: require("../assets/card.jpg"), title: "Customer 3" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.head {
+  height: 50vh;
+  background: url("../assets/desert.jpg") no-repeat center center;
+  background-size: cover;
+}
+#team {
+  padding: 2rem 0;
+}
+.theTeam {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  margin: 3rem 0;
+}
+.person {
+  margin: 1rem 0;
+}
+.person img {
+  border-radius: 50%;
+  height: 150px;
+  width: 150px;
+}
+#work {
+  display: flex;
+  flex-wrap: wrap;
+  background-color: rgb(51, 51, 51);
+  color: white;
+  padding: 3rem 1rem;
+  gap: 0.8rem;
+  position: relative;
+}
+#work .fas {
+  position: absolute;
+  bottom: -15px;
+  right: 1rem;
+  background-color: rgb(0, 119, 119);
+  padding: 0.5rem;
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  font-size: 1.3rem;
+  font-weight: 700;
+}
+.item {
+  flex: 1 1 250px;
+}
+.work-desc {
+  text-align: left;
+}
+.work-card {
+  background-color: white;
+  height: 500px;
+}
+.work-card img {
+  height: 50%;
+  width: 100%;
+  object-fit: cover;
+}
+.card-text {
+  text-align: left;
+  padding: 0.5rem;
+  color: black;
+}
+#contact {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  text-align: left;
+  padding: 3rem 1rem;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 0 5px black;
+  padding: 1.5rem 1rem;
+}
+.form input {
+  border: 2px rgb(182, 182, 182);
+  border-style: none none solid none;
+  margin: 0.5rem 0;
+}
+.form input:focus {
+  outline: none;
+}
+.form label {
+  font-weight: 600;
+  margin: 0.5rem 0;
+}
+.sub {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 0;
+}
+.sub button {
+  background-color: black;
+  color: white;
+  border: none;
+  padding: 0.5rem 0.7rem;
+  cursor: pointer;
+}
+.sub input {
+  margin: 0.5rem;
+}
+.info h2 {
+  border-bottom: 5px solid rgb(0, 119, 119);
+  width: fit-content;
+}
+.info .fas,
+.far {
+  color: rgb(0, 119, 119);
+}
+</style>
